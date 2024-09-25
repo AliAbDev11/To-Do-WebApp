ca98d95432125cd4402ec69f3a2e3c16ff84ba7e
Ali AB <aliabdev07@gmail.com>
2024-09-25 02:35:08 +0100

    create forms.py
    - add tasklist and task form

---
+++ todolist/todo_app/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import *
+
+class TaskListForm(forms.ModelForm):
+    
+    class Meta:
+        model = TaskList
+        fields = ['title']
+    
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if TaskList.objects.filter(title=title).exists():
+            raise forms.ValidationError("Task List with this title already exists.")
+        return title
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'completed', 'tasklist']  # Specify the fields to include in the form
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Task Title'}),
+            'completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'tasklist': forms.Select(attrs={'class': 'form-select'}),
+        }
