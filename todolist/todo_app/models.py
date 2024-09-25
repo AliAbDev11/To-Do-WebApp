548858c304ed52a1c859a04f9f21b39538bd857e
Ali AB <aliabdev07@gmail.com>
2024-09-25 02:29:06 +0100

    Update models.py
    - add task model

--- todolist/todo_app/models.py
+++ todolist/todo_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class TaskList(models.Model):
+    title = models.CharField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.title
+
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+    tasklist = models.ForeignKey('TaskList', on_delete=models.CASCADE)  # Assuming a TaskList relation
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
