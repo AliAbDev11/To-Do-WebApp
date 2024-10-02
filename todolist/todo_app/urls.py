c6b81a682a2b6658a1ee6b449b201ffa3c6cf121
Ali AB <aliabdev07@gmail.com>
2024-10-02 15:47:57 +0100

    Update urls.py
    - Add toggle_task_status, delete_task, and update_task

--- todolist/todo_app/urls.py
+++ todolist/todo_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('tasklist/', views.tasklist, name='tasklist'),  # This is the POST action URL
     path('tasks/<int:id>/', views.task_detail, name='task_detail'),  # This is the task detail view
-
+    path('toggle-task-status/<int:task_id>/', views.toggle_task_status, name='toggle_task_status'),
+    path('delete-task/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('update-task/<int:task_id>/', views.update_task, name='update_task'),
 ]
