af4f9d766e3080b326cc8ae2552a1d8e64a17ed6
Ali AB <aliabdev07@gmail.com>
2024-11-28 02:02:06 +0100

    Update models.py
    - Add user foreign key in task list class

--- todolist/todo_app/models.py
+++ todolist/todo_app/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class TaskList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=50,unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -11,9 +15,11 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
     tasklist = models.ForeignKey('TaskList', on_delete=models.CASCADE)  # Assuming a TaskList relation
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
