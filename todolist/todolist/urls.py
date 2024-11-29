5dda1d7432f2e8a91f8c0ed0d141effe5ee7696f
Ali AB <aliabdev07@gmail.com>
2024-11-29 02:18:19 +0100

    Update todolist/urls.py
    - Add handler404

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
+from authentication.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo_app.urls')),
     path('', include('authentication.urls')),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+handler404 = 'authentication.views.page_not_found'
